8083bf37f768c88da1a8ce5f095fa2310ce428e5
Milt0n <cedric.lood@gmail.com>
2015-07-18 22:50:15 +0200

    update provisioning script to create mongodb collection

--- BaseConfigVM.sh
+++ BaseConfigVM.sh
@@ -5,3 +5,13 @@ echo 'provisioning.. '
 sudo apt-get update
 sudo apt-get install -y nodejs npm mongodb
 echo '.. done!'
+
+# database creation
+DB_NAME=whoviz
+CSV_WHO=/vagrant/data/WHO.csv
+
+echo 'Creation of mongoDB $DB_NAME.. '
+if [ ! -e /data/db/$DB_NAME.ns ]; then
+    `mongoimport --db $DB_NAME --collection who --type csv --headerline --file $CSV_WHO`
+fi
+echo '.. done!'
