54f91728e2c52f491d9fb55d9c0f34d694ac9b69
Milt0n <cedric.lood@gmail.com>
2015-08-20 22:31:11 +0200

    nodejs script to process cor matrix and limit its content

---
+++ data/cor/build_cor_json.js
@@ -0,0 +1,118 @@
+var fs = require('fs');
+var cor, neg_cor = [], pos_cor = [];
+
+var resp = [],
+    resn = [];
+
+fs.readFile('./cor.json', 'utf8', function(err, c) {
+    if(err) throw err;
+    cor = JSON.parse(c);
+    // cleaning up objects irrelevant to cor matrix
+    for(var i = 0; i < cor.length; i++) {
+        delete cor[i]['_id'];
+    }
+    
+    var line;
+    for(var i = 0; i < cor.length; i++){
+        line = 0;
+        for(var key in cor[i]){
+            if (line == 0)
+                var header = cor[i][key];
+            if(line>i){
+                break; // eliminates upper diagonal of cor matrix (redundant info)
+            }
+            else {
+                if(cor[i][key] > 0.6){
+                    pos_cor.push({"var1":key, "var2":header, "cor":cor[i][key]});
+                }
+                if(cor[i][key] < -0.6){
+                    neg_cor.push({"var1":key, "var2":header, "cor":cor[i][key]});
+                }
+            }
+            line++;
+        }
+    }
+
+    pos_cor.sort(function(a, b){
+        return b.cor-a.cor;
+    });
+
+    neg_cor.sort(function(a, b){
+        return a.cor-b.cor;
+    });
+
+    var sizep = Math.ceil(Math.sqrt(pos_cor.length)),
+        sizen = Math.ceil(Math.sqrt(neg_cor.length));
+
+
+    while (pos_cor.length > 0)
+        resp.push(pos_cor.splice(0, 50));
+    
+    while (neg_cor.length > 0)
+        resn.push(neg_cor.splice(0, 50));
+    
+    save_results();
+});
+
+function save_results(){
+    fs.writeFile("pos_cor.json", JSON.stringify(resp, null, 4), function(err) {
+        if(err) {
+            console.log(err);
+        } else {
+            console.log("JSON saved");
+        }
+    }); 
+    fs.writeFile("neg_cor.json", JSON.stringify(resn, null, 4), function(err) {
+        if(err) {
+            console.log(err);
+        } else {
+            console.log("JSON saved");
+        }
+    }); 
+}
+
+
+// fs.readFile('./cor.json', 'utf8', function(err, c) {
+//     if(err) throw err;
+//     cor = JSON.parse(c);
+//     // cleaning up objects irrelevant to cor matrix
+//     for(var i = 0; i < cor.length; i++) {
+//         delete cor[i]['_id'];
+//         //delete cor[i][""];
+//     }
+
+//     // remove upper correlation matrix (redundant)
+//     delete cor[0];
+//     var j = 1;
+//     for(var i = 0; i < cor.length; i++) {
+//         for(var key in cor[i]){
+//             if(j < i){
+//                 j++;
+//                 continue;
+//             }
+//             delete cor[i][key];
+//         }
+//     }
+//     console.log(cor.length * cor.length);
+//     further_cleanup();
+// });
+
+// function further_cleanup(){
+//     var l = cor.length;
+//     for(var i=0; i< l; i++){
+//         for(var key in cor[i]){
+//             if (cor[i][key] > -0.5 && cor[i][key] < 0.5)
+//                 delete cor[i][key];
+//         }
+//     } 
+//     var count = 0;
+//     var t = cor.length;
+//     for(var i=0; i< t; i++){
+//         for(var key in cor[i]){
+//             count++;
+//         }
+//     } 
+//     console.log(count);
+//     console.log(cor.length);
+//     save_results();
+// }
